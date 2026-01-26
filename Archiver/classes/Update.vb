babf24defa391c57182815fdad6b5da34c852932
BasicNullification <36026282+BasicNullification@users.noreply.github.com>
2026-01-25 19:33:40 -0600

    Add COM attributes

--- Archiver/classes/Update.vb
+++ Archiver/classes/Update.vb
@@ -1,7 +1,11 @@
-﻿Public Interface IUpdate
+﻿<ComVisible(True), InterfaceType(ComInterfaceType.InterfaceIsDual)>
+<Guid("F78D7E0B-06B8-4AE9-A041-EF1430546489")>
+Public Interface IUpdate
     Inherits ISevenZipBase
 End Interface
 
+<ComVisible(True), ClassInterface(ClassInterfaceType.None)>
+<Guid("E7C3102B-95EC-4F3B-AC63-A23C7AE6FF6F")>
 Public Class Update
 
     Inherits SevenZipBase
